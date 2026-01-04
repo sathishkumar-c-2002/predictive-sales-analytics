9c48728aa199df87061ffd762e130b1ebce97a91
sathish <csathishkumar2002@gmail.com>
2026-01-04 23:49:37 +0530

    Remove --user flag for Render virtualenv

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,6 @@ echo "Installing Node.js dependencies..."
 yarn install
 
 echo "Installing Python dependencies..."
-pip3 install --user pandas numpy scikit-learn joblib || pip install --user pandas numpy scikit-learn joblib
+pip install pandas numpy scikit-learn joblib
 
 echo "Build complete!"
